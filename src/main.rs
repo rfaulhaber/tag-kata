e3d3235838a5963195eacf48dcdb2f18655ada68
Ryan Faulhaber <faulhaberryan@gmail.com>
2019-02-14 21:44:44 -0500

    implemented iterator object

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,40 @@
+extern crate serde_yaml;
+
 mod tag;
 
+use serde_yaml::Value;
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let filename = match args.get(1) {
+        Some(path) => path,
+        None => {
+            eprintln!("no file specified");
+            process::exit(1);
+        }
+    };
+
+    println!("reading from: {}", filename);
+
+    let file = match File::open(filename) {
+        Ok(f) => f,
+        Err(reason) => {
+            eprintln!("could not open {} due to: {}", filename, reason);
+            process::exit(2);
+        }
+    };
+
+    let systems: Value = match serde_yaml::from_reader(&file) {
+        Ok(value) => value,
+        Err(reason) => {
+            eprintln!("could not parse {} due to: {}", filename, reason);
+            process::exit(1);
+        }
+    };
+
+    println!("{:?}", systems);
 }
--- src/tag.rs
+++ src/tag.rs
@@ -1,30 +1,65 @@
 use std::collections::HashMap;
 
 pub struct TagSystem<'a> {
-	n: u64,
-	alphabet: Vec<&'a str>,
 	rules: HashMap<&'a str, &'a str>,
+	previous_word: String,
 }
 
 impl<'a> TagSystem<'a> {
-	fn new(n: u64, alphabet: Vec<&'a str>) -> TagSystem<'a> {
+	fn new(start: String) -> TagSystem<'a> {
 		TagSystem {
-			n,
-			alphabet,
 			rules: HashMap::new(),
+			previous_word: start,
 		}
 	}
 
 	fn add_rule(&mut self, rule: &'a str, mapping: &'a str) {
 		self.rules.insert(rule, mapping);
 	}
+}
 
-	fn next_word(&self, previous_word: String) -> Option<String> {}
+impl<'a> Iterator for TagSystem<'a> {
+	type Item = String;
+
+	fn next(&mut self) -> Option<Self::Item> {
+		let tag = self.previous_word.get(0..1).unwrap();
+		let append = match self.rules.get(tag) {
+			Some(next) => String::from(*next).to_owned(),
+			None => return None,
+		};
+
+		let rest = match self.previous_word.get(2..) {
+			Some(r) => String::from(r).to_owned(),
+			None => return None,
+		};
+
+		self.previous_word = rest + &append;
+
+		Some(self.previous_word.clone())
+	}
 }
 
 mod test {
 	use super::*;
 
 	#[test]
-	fn given_rules_system_should_produce_next_iteration() {}
+	fn given_rules_system_should_produce_next_iteration() {
+		let mut system = TagSystem::new(String::from("baa"));
+
+		/*
+		a  -->  ccbaH
+		 b  -->  cca
+		 c  -->  cc
+		*/
+		system.add_rule("a", "ccbaH");
+		system.add_rule("b", "cca");
+		system.add_rule("c", "cc");
+
+		assert_eq!(system.next(), Some(String::from("acca")));
+		assert_eq!(system.next(), Some(String::from("caccbaH")));
+		assert_eq!(system.next(), Some(String::from("ccbaHcc")));
+		assert_eq!(system.next(), Some(String::from("baHcccc")));
+		assert_eq!(system.next(), Some(String::from("Hcccccca")));
+		assert_eq!(system.next(), None);
+	}
 }
